fa9762025fa12c6301e5e89b77844dcc65231a0b
Kpennie <kpennie0@gmail.com>
2025-06-17 00:06:59 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import "./assets/img/4geeks.ico";
 
 
           //. 0            1         2             3           4
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'  'Bobby'];
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'Bobby'];
 let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
